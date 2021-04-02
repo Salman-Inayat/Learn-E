6a7359c3c5c2a198058a6ccec5b8d3521aedca54
Salman-Inayat <salmaninayat1999@gmail.com>
2021-04-02 13:55:34 +0500

    component files added

---
+++ src/components/TermsPage/index.js
@@ -0,0 +1,29 @@
+ import React from 'react'
+import './style.css'
+
+ function TermsPage(){
+     return(
+        <div className='container terms-container' >
+        <div className="title">
+            <h2>Terms And Conditions</h2>
+            <span>&nbsp;</span>
+        </div>
+        <br/>
+        <p>Following terms and conditions will be followed by all the users of our website and mobile application. If at any point, you do not agree to the terms and conditions designed by us, please do not use the application.</p>
+        <h4>Guarantees</h4>
+        <p>Our application guarantees the privacy of its users and the work given in their personal portals. Also, we do not acquire any information from the users through this application, therefore we do not spread any personal information out of our premises. We also guarantee to provide you with updated content that has been uploaded by the school so that no child suffers a loss of their work.</p>
+        <h4>Copyright</h4>
+        <p>The content given in our application is completely collective and fall under our copyrights. Due to this reason, we do not allow users or non-users to use it elsewhere. You can print the screen or take screenshots while using it for your school or the application purpose. You also further agree to not delete or change any of the proprietary material or notices that have been downloaded from our app.</p>
+        <h4>Registered logo</h4>
+        <p>Our logo has been registered and thus our service marks cannot be used by elsewhere, which will be counted as illegal.</p>
+        <h4>Limitation of Liability</h4>
+        <p>We are not liable if you share your login credentials with some third person and cause mishaps related to your work.</p>
+        <h4>Terms and Terminations</h4>
+        <p>These terms and conditions are only applied to the people that will be using this application. You can stop using this app any time and the terms and conditions won't apply to you then. Though the provisions that relate to copyrights, disclaimer, limitations will surely be served as termination.</p>
+        <h4>Use of the application</h4>
+        <p>This application is supposed to be used for the means of education, and if any kind of mishap happens, you are likely to get banned from the use of this application</p>
+    </div>
+     )
+ }
+
+ export default TermsPage;
